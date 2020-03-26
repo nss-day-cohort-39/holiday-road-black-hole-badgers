4270c60f6461bcc360393d361d632d85b3324fb3
Hellowalls1 <thesingingbutcher@gmail.com>
2020-03-26 15:09:49 -0500

    added the dropdown for the attractions (bizzaries)/ included setting up the attraction api

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
-import { getParks } from "./parks/ParkProvider.js";
+// import { getParks } from "./parks/ParkProvider.js";
+import { getAttractions } from "./attractions/AttractionProvider.js";
+import AttractionSelect from "./attractions/AttractionSelect.js";
 
-getParks()
+
+getAttractions()
+    .then(AttractionSelect)
+    
+// getParks()
