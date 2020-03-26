72d16fde4394e457413fd4fb2ce2dd39bc8c3c8c
TylerSerafin <tyler.serafin2@gmail.com>
2020-03-26 11:58:54 -0500

    throw-away commit

---
+++ scripts/parks/ParkProvider.js
@@ -0,0 +1,16 @@
+import settings from "../.Settings.js"
+
+let parks = []
+export const useParks = () => {
+    return parks.slice()
+}
+export const getParks = () => {
+    return fetch(`https://developer.nps.gov/api/v1/parks?api_key=${settings.npsKey}&stateCode=TN`)
+        .then(response => response.json())
+        .then(
+            parsedParks => {
+                parks = parsedParks
+                console.table(parks)
+            }
+            )
+        }
