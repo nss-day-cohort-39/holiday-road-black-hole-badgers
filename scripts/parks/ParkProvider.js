b0e14accfb9485cd9a3471ba3751295c617c1f8f
TylerSerafin <tyler.serafin2@gmail.com>
2020-03-26 15:04:34 -0500

    Madethe park dropdown

--- scripts/parks/ParkProvider.js
+++ scripts/parks/ParkProvider.js
@@ -1,15 +1,16 @@
-import settings from "../.Settings.js"
+import settings from "../../Settings.js"
 
 let parks = []
+
 export const useParks = () => {
     return parks.slice()
 }
 export const getParks = () => {
-    return fetch(`https://developer.nps.gov/api/v1/parks?api_key=${settings.npsKey}&stateCode=TN`)
+    return fetch(`https://developer.nps.gov/api/v1/parks?api_key=${settings.npsKey}&stateCode=CA,AZ,NV,UT,CO,NM`)
         .then(response => response.json())
         .then(
             parsedParks => {
-                parks = parsedParks
+                parks = parsedParks.data
                 console.table(parks)
             }
             )
