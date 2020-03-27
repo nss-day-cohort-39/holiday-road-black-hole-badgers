e810e73d0e84f934969c6b9e3a48c3e68ed376e9
root <root@DESKTOP-2KLJLBF.localdomain>
2020-03-27 11:53:26 -0400

    Moved event add event listener out of the dropdown function.

--- scripts/attractions/AttractionSelect.js
+++ scripts/attractions/AttractionSelect.js
@@ -1,27 +1,26 @@
 import { useAttractions } from "./AttractionProvider.js"
 
-
-
-
 // Get a reference to the DOM element where the <select> will be rendered
 const contentTarget = document.querySelector(".attractionDropdown")
 const eventHub = document.querySelector(".container")
+
+contentTarget.addEventListener("change", changeEvent => {
+    if (changeEvent.target.id ==="attractionSelect") {
+        const attractionChosen = changeEvent.target.value
+        const attractionChosenEvent = new CustomEvent ("attractionChosen", {
+            detail: {
+                eatery: attractionChosen
+            }
+        })
+        eventHub.dispatchEvent(attractionChosenEvent)
+    }
+})
 
 const AttractionSelect = () => {
     // Get all attractions from application state
     const attractions = useAttractions()
 
-    contentTarget.addEventListener("change", changeEvent => {
-        if (changeEvent.target.id ==="attractionSelect") {
-            const attractionChosen = changeEvent.target.value
-            const attractionChosenEvent = new CustomEvent ("attractionChosen", {
-                detail: {
-                    eatery: attractionChosen
-                }
-            })
-            eventHub.dispatchEvent(attractionChosenEvent)
-        }
-    })
+
 
 
 
