1164e5ce447a80cb368ebec112b7bcad49448de5
root <root@DESKTOP-2KLJLBF.localdomain>
2020-03-27 16:56:55 -0400

    Working on diaplaying weather.

--- scripts/weather/WeatherList.js
+++ scripts/weather/WeatherList.js
@@ -1,14 +1,23 @@
 import { getWeather, useWeather } from "./WeatherProvider.js"
 
+
 export const weatherToDisplay = () => {
-
-    let allWeather = useWeather()
-    const checkHours = () => {
-        
-    }
-
-    for (const threeHourWeatherGap of allWeather) {
-        threeHourWeatherGap.filter()
-    }
-    
+let allWeather = useWeather()
+debugger
+const FilteredWeather = allWeather.filter(weather => allWeather.dt_txt.endswith("12:00:00"))
+console.log(FilteredWeather)
 }
+
+// export const weatherToDisplay = () => {
+
+//     let allWeather = useWeather()
+    
+//     const checkHours = () => {
+        
+//     }
+
+//     for (const threeHourWeatherGap of allWeather) {
+//         threeHourWeatherGap.filter()
+//     }
+    
+// }
