416ed019a797b837d7de689b4173eb55e962dc59
kristen-howton <kristen.howton@gmail.com>
2020-03-30 13:53:40 -0500

    fixing merge conflicts

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,13 @@
-import { getParks } from "./parks/ParkProvider.js";
-import { parkSelect } from "./parks/ParkSelect.js";
-import { getAttractions } from "./attractions/AttractionProvider.js";
-import AttractionSelect from "./attractions/AttractionSelect.js";
-import { getEateries } from "./eateries/EateryProvider.js";
-import { eaterySelect } from "./eateries/EaterySelect.js";
-import { getWeather, useWeather } from "./weather/WeatherProvider.js";
-import  "./eateries/EateryCard.js";
-import { WeatherList } from "./weather/WeatherList.js";
+import { getParks } from "./parks/ParkProvider.js"
+import { parkSelect } from "./parks/ParkSelect.js"
+import { getAttractions } from "./attractions/AttractionProvider.js"
+import AttractionSelect from "./attractions/AttractionSelect.js"
+import { getEateries } from "./eateries/EateryProvider.js"
+import { eaterySelect } from "./eateries/EaterySelect.js"
+import { getWeather, useWeather } from "./weather/WeatherProvider.js"
+import  "./eateries/EateryCard.js"
+import { WeatherList } from "./weather/WeatherList.js"
+import "./attractions/AttractionCard.js"
 
 getAttractions()
     .then(AttractionSelect)
@@ -19,3 +20,5 @@ getEateries()
 
 getWeather()
     .then(WeatherList)
+
+
