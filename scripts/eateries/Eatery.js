28f57f099bde6c73051c493c8fb41a8089ce2a5a
root <root@DESKTOP-2KLJLBF.localdomain>
2020-03-30 15:12:34 -0400

    Removed function keyword, an extra quotation mark, and added eatery details text to the button.

--- scripts/eateries/Eatery.js
+++ scripts/eateries/Eatery.js
@@ -1,8 +1,8 @@
 export const eateryComponent = (eateryObj) => {
    return `
-        <div class="eateryList"">
+        <div class="eateryList">
             <h3 class="eateryName">${eateryObj.businessName}</h3>
-            <button class="detailsButton" id="eateryDetailsButton--${eateryObj.id}">Details</button>
+            <button class="detailsButton" id="eateryDetailsButton--${eateryObj.id}">Eatery Details</button>
         </div>
    `
 }
