1a71cbfc9ca115c513bffe2735fc9c4a71cb988d
root <root@DESKTOP-2KLJLBF.localdomain>
2020-03-31 17:02:41 -0400

    Finding the zip code of the selected park.

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -1,10 +1,25 @@
 import settings from "../../Settings.js"
+import { useParks } from "../parks/ParkProvider.js"
+
 //empty array that stores the weather, must be defined to store the data
 let weather = []
-
+const eventHub = document.querySelector(".container")
 // Allow other modules to get a copy of the weather data
 
+eventHub.addEventListener("parkChosen", event => {
+    const allParks = useParks()
+    const thePark = event.detail.park
+    const chosenPark = allParks.find(
+        (currentPark) => {
+            if (currentPark.name === thePark) {
+                return currentPark
+            }
+        }
+    )
+    const currentZip = chosenPark.addresses[0].postalCode
+})
 
+// addresses[0].postalCode
 export const useWeather = () => {
    return weather.slice()
 }
@@ -13,7 +28,7 @@ export const useWeather = () => {
 // Get weather data state from API
 
 export const getWeather = () => {
-    return fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=94016,us&units=imperial&appid=${settings.weatherKey}`)
+    return fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${currentZip},us&units=imperial&appid=${settings.weatherKey}`)
         .then(response => response.json())
         .then(
             parsedWeather => {
