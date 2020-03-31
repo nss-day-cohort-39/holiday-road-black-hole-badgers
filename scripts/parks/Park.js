83302e48b13920c210302f5db15c09b4ad1a00bc
kristen-howton <kristen.howton@gmail.com>
2020-03-31 13:13:56 -0500

    got dialog working

--- scripts/parks/Park.js
+++ scripts/parks/Park.js
@@ -3,6 +3,7 @@ export const parkComponent = parkObject => {
         <article class="parkList">
             <h3 class="parkName">${parkObject.name}</h3>
             <section>${parkObject.addresses[0].city}, ${parkObject.addresses[0].stateCode}</section>
+            <button class="detailsButton" id="parkDetailsButton--${parkObject.id}">Park Details</button>
             <dialog class="dialog--park" id="details--${parkObject.id}">
                 <ul>
                     <li>Overview: ${parkObject.description}</li>
@@ -11,7 +12,6 @@ export const parkComponent = parkObject => {
                 </ul>
                 <button class="button--close">Close Park Details</button>
             </dialog>
-            <button class="detailsButton" id="parkDetailsButton--${parkObject.id}">Park Details</button>
         </article>
      `
 }
