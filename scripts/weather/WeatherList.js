c99a17b4b5c2540e3f540055feca49116dfd6f33
root <root@DESKTOP-2KLJLBF.localdomain>
2020-03-31 11:15:02 -0400

    Made comments for the weather filter.

--- scripts/weather/WeatherList.js
+++ scripts/weather/WeatherList.js
@@ -8,7 +8,7 @@ export const FilterWeather = () => {
     // Get the weather
     let weatherToDisplay = useWeather()
 
-        // Filter the list of weather
+        // Filter the list of weather by selecting each day's data at 12 o'clock, results in 5 days
         weatherToDisplay = weatherToDisplay.filter(weather => {
             if (weather.dt_txt.includes("12:00:00")) {
                 return true
@@ -19,7 +19,7 @@ export const FilterWeather = () => {
 }
 
 
-// Renders all 40 weather objects
+// Render the weather objects
 
 export const render = weatherToRender => {
     contentTarget.innerHTML = weatherToRender.map(
