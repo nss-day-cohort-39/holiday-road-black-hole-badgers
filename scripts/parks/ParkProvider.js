33bfc3875dd4140902087892a9a2103cc6237c1a
TylerSerafin <tyler.serafin2@gmail.com>
2020-04-03 09:48:43 -0500

    I removed the local parks data and setup parks api

--- scripts/parks/ParkProvider.js
+++ scripts/parks/ParkProvider.js
@@ -9,11 +9,11 @@ export const useParks = () => {
 
 //1. fetch parks from the API and store it in the parks variable and sorts the dropdown in ABC order
 export const getParks = () => {
-    return fetch(`http://localhost:8090/data`)
+    return fetch(`https://developer.nps.gov/api/v1/parks?api_key=${settings.npsKey}&stateCode=CA,AZ,NV,UT,CO,NM`)
         .then(response => response.json())
         .then(
             parsedParks => {
-                parks = parsedParks.sort((Beginning, End) => {
+                parks = parsedParks.data.sort((Beginning, End) => {
                     let nameA = Beginning.states
                     let nameB = End.states
                     if (nameA < nameB) {
